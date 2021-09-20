fd440233b14428685deb9c0d9a92de097f3becc5
Aleksander-Kovalenko <coldfellow@gmail.com>
2021-09-20 19:59:47 +0300

    исправил

--- js/task-06.js
+++ js/task-06.js
@@ -9,7 +9,12 @@ refs.inputField.addEventListener("blur", calculateSymbol);
 function calculateSymbol(e) {
   const maxLength = refs.inputField.getAttribute("data-length");
   const userVal = e.target.value.length;
-  if (userVal > maxLength || userVal < maxLength)
+  console.log(userVal);
+  const validName = userVal == maxLength;
+  if (!validName) {
     refs.inputField.classList.add("invalid");
-  refs.inputField.classList.add("valid");
+  } else {
+    refs.inputField.classList.remove("invalid");
+    refs.inputField.classList.add("valid");
+  }
 }
